754a1295656fe45580b6f00a39a604cd5d529c9e
Thomas Erhel <thomas.erhel@gmail.com>
2020-11-06 17:05:45 +0100

    Add voice, $hello command, logging

--- index.py
+++ index.py
@@ -1,19 +1,26 @@
 import discord
 import json
+import logging
 
 with open('./config.json') as config_file:
     data = json.load(config_file)
 
 token = data['token']
+client = discord.Client()
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 
-client = MyClient()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 client.run(token)
+logging.basicConfig(level=logging.INFO)
